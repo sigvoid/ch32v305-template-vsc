62feae69a05cdec90b668ba0fb81d051fdcac5b8
sigvoid <git@0u0.in>
2024-02-21 21:07:41 +0800

    fix warning

--- User/main.c
+++ User/main.c
@@ -56,8 +56,8 @@ int main(void)
     SystemCoreClockUpdate();
     Delay_Init();
     USART_Printf_Init(115200);	
-    printf("SystemClk:%d\r\n", SystemCoreClock);
-    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+    printf("SystemClk:%ld\r\n", SystemCoreClock);
+    printf( "ChipID:%08lx\r\n", DBGMCU_GetCHIPID() );
 
     printf("GPIO Toggle TEST\r\n");
     GPIO_Toggle_INIT();
